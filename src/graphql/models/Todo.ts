5d50d7555247a03155b2d003dfdbe784039942cb
Vasyl Boychuk <vasyl.boychuk@panion.org>
2024-08-26 10:59:13 +0300

    add todo model

---
+++ src/graphql/models/Todo.ts
@@ -0,0 +1,30 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToMany,
+  JoinTable,
+} from 'typeorm';
+import { User } from './User';
+
+@Entity({ name: 'todos' })
+@ObjectType()
+export class Todo {
+  @PrimaryGeneratedColumn()
+  @Field(() => Int, { description: "The todo's unique identifier" })
+  id: number;
+
+  @Column()
+  @Field(() => String, { description: "The todo's title" })
+  title: string;
+
+  @Column()
+  @Field(() => Int, { description: 'The number of points for this todo' })
+  points: number;
+
+  @ManyToMany(() => User, (user) => user.todos)
+  @JoinTable()
+  @Field(() => [User], { description: 'Users associated with this todo' })
+  users: User[];
+}
--- src/graphql/models/User.ts
+++ src/graphql/models/User.ts
@@ -5,8 +5,10 @@ import {
   PrimaryGeneratedColumn,
   OneToOne,
   JoinColumn,
+  ManyToMany,
 } from 'typeorm';
 import { UserSetting } from './UserSetting';
+import { Todo } from './Todo';
 
 @Entity({ name: 'users' })
 @ObjectType()
@@ -29,6 +31,10 @@ export class User {
 
   @OneToOne(() => UserSetting)
   @JoinColumn()
-  @Field({ nullable: true })
+  @Field(() => UserSetting, { nullable: true })
   settings?: UserSetting;
+
+  @ManyToMany(() => Todo, (todo) => todo.users)
+  @Field(() => [Todo], { description: 'Todos associated with this user' })
+  todos: Todo[];
 }
